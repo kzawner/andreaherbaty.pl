ac7f6dd91e5f7e6b547bff1656dff832ee8b2604
Kzawner <kzawner@gmail.com>
2013-06-01 12:32:58 +0300

    Config added

---
+++ upload/config.php
@@ -0,0 +1,21 @@
+<?php
+// DIR
+define('DIR_APPLICATION', '/home/abbyz/www/teastore/upload/catalog/');
+define('DIR_SYSTEM', '/home/abbyz/www/teastore/upload/system/');
+define('DIR_DATABASE', '/home/abbyz/www/teastore/upload/system/database/');
+define('DIR_LANGUAGE', '/home/abbyz/www/teastore/upload/catalog/language/');
+define('DIR_TEMPLATE', '/home/abbyz/www/teastore/upload/catalog/view/theme/');
+define('DIR_CONFIG', '/home/abbyz/www/teastore/upload/system/config/');
+define('DIR_IMAGE', '/home/abbyz/www/teastore/upload/image/');
+define('DIR_CACHE', '/home/abbyz/www/teastore/upload/system/cache/');
+define('DIR_DOWNLOAD', '/home/abbyz/www/teastore/upload/download/');
+define('DIR_LOGS', '/home/abbyz/www/teastore/upload/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'teacart');
+define('DB_PASSWORD', 'n2g7Nyu');
+define('DB_DATABASE', 'teacart');
+define('DB_PREFIX', 'oc_');
+?>
